67f9070485be51319f505bc654b39eb788c99447
Catarina <catarina_pine@hotmail.com>
2024-07-23 01:32:28 -0300

    add recomendacao de filmes

--- avaliacao_de_filmes.py
+++ avaliacao_de_filmes.py
@@ -1,45 +1,43 @@
 import abstra.forms as af
 import abstra.tables as at
 import abstra.workflows as aw
-import random
 
 informacoes = af.Page().display_markdown(
-"""
+    """
 ## 👋 Olá! Seja bem vindo ao avaliador de Filmes Sciety\n 
 ### Antes de iniciar a avaliação precisamos de alguns dados.
 
 """)\
-                .read("Nome e sobrenome", key="name")\
-                .read_email("Email", key="email")\
-                .run("Próximo")
+    .read("Nome e sobrenome", key="name")\
+    .read_email("Email", key="email")\
+    .run("Próximo")
 
 name, email = informacoes["name"], informacoes["email"]
 
 # Salvar usuario no banco de dados
 id_usuario = at.insert("usuarios", {"nome": name, "email": email})["id"]
 
-# ler lista de filmes e randomizar
+# ler lista de filmes
 lista_filmes = at.select("filmes")
-random.shuffle(lista_filmes)
 
 nome_filmes = [filme["nome"] for filme in lista_filmes]
 
 # Criar Pagina para avaliar os filmes
 pagina_avaliacoes = af.Page()
-for filme in nome_filmes[:8]:
-    avaliacao = pagina_avaliacoes.read_rating(f"Qual sua nota para {filme}?")
-    
+for filme in nome_filmes:
+    avaliacao = pagina_avaliacoes.read_rating(
+        f"Qual sua nota para {filme}?", required=False)
+
 avaliacoes = pagina_avaliacoes.run("Enviar")
 
 # Salvar as avaliações no banco de dados
 for filme, avaliacao in zip(nome_filmes, avaliacoes):
-    nota = avaliacao["nota"]
+    nota = avaliacoes[avaliacao]
     id_filme = [f["id"] for f in lista_filmes if f["nome"] == filme][0]
-    at.insert("avaliacoes", {"id_usuario": id_usuario, 
-                             "id_filme": id_filme, 
+    at.insert("avaliacoes", {"id_usuario": id_usuario,
+                             "id_filme": id_filme,
                              "nota": nota})
 
 
 # Enviar informações do usuario e dos filmes não avaliados para o sistema de recomendação
 aw.set_data("usuario", {"id": id_usuario, "nome": name, "email": email})
-aw.set_data("filmes", [f for f in lista_filmes if f["nome"] not in nome_filmes])
--- sistema_de_recomendacao.py
+++ sistema_de_recomendacao.py
@@ -1,4 +1,105 @@
-import abstra.workflows as aw
+import abstra.tables as at
+import pandas as pd
+from abstra.messages import send_email
+from sklearn.metrics.pairwise import cosine_similarity
 
-usuarios = aw.get_data("usuarios")
-filmes = aw.get_data("filmes")
+
+def coletar_e_preparar_avaliacoes():
+    avaliacoes = at.select("avaliacoes")
+    avaliacoes_df = pd.DataFrame(avaliacoes)
+    matriz_usuario_filme = avaliacoes_df.pivot_table(
+        index='id_usuario', columns='id_filme', values='nota').fillna(0)
+    return matriz_usuario_filme
+
+
+def calcular_similaridade(matriz_usuario_filme):
+    similaridade_usuarios = cosine_similarity(matriz_usuario_filme)
+
+    # Convertendo para DataFrame
+    similaridade_usuarios_df = pd.DataFrame(
+        similaridade_usuarios, index=matriz_usuario_filme.index, columns=matriz_usuario_filme.index)
+
+    return similaridade_usuarios_df
+
+
+def calcular_similaridade(matriz_usuario_filme):
+    # Preenchimento de NaNs com 0 para simplificação
+    matriz_usuario_filme = matriz_usuario_filme.fillna(0)
+
+    # Calculando similaridades
+    similaridade_usuarios = cosine_similarity(matriz_usuario_filme)
+
+    # Convertendo para DataFrame
+    similaridade_usuarios_df = pd.DataFrame(
+        similaridade_usuarios, index=matriz_usuario_filme.index, columns=matriz_usuario_filme.index)
+
+    return similaridade_usuarios_df
+
+
+def recomendar_filmes(usuario_alvo, matriz_aval, similaridade_usuarios_df, filmes_dict, num_recommendacoes=2):
+    if usuario_alvo not in similaridade_usuarios_df.index:
+        return pd.Series()
+    # Similaridade do usuário alvo com outros usuários
+    similaridades = similaridade_usuarios_df[usuario_alvo].sort_values(
+        ascending=False)
+
+    # Filmes que o usuário não assistiu
+    filmes_nao_assistidos = matriz_aval.loc[usuario_alvo][matriz_aval.loc[usuario_alvo] == 0].index
+
+    recomendacoes = {}
+
+    for filme in filmes_nao_assistidos:
+        soma_similaridades = 0
+        sum_weighted_ratings = 0
+
+        for usuario in similaridades.index:
+            if matriz_aval.loc[usuario, filme] > 0:  # Usuário avaliou o filme
+                similarity_score = similaridades[usuario]
+                soma_similaridades += similarity_score
+                sum_weighted_ratings += similarity_score * \
+                    matriz_aval.loc[usuario, filme]
+
+        if soma_similaridades != 0:
+            recomendacoes[filme] = sum_weighted_ratings / soma_similaridades
+        else:
+            recomendacoes[filme] = 0
+
+    # Pegando os nomes dos filmes ao invés dos IDs
+    recomendacoes_nomes = {
+        filmes_dict[filme]: avaliacao for filme, avaliacao in recomendacoes.items()}
+
+    # Ordenar recomendações
+    recomendacoes_nomes = pd.Series(recomendacoes_nomes).sort_values(
+        ascending=False).head(num_recommendacoes)
+    return recomendacoes_nomes
+
+
+def formatar_recomendacoes(recomendacoes, usuario_alvo):
+    message = f"Recomendações de filmes para o usuário {usuario_alvo}:\n"
+    message += "-" * 40 + "\n"
+
+    for i, (filme, avaliacao) in enumerate(recomendacoes.items(), 1):
+        message += f"{i}. Filme: {filme} | Avaliação estimada: {avaliacao:.2f}\n"
+
+    message += "-" * 40
+    return message
+
+
+matriz_usuario_filme = coletar_e_preparar_avaliacoes()
+similaridade_usuarios_df = calcular_similaridade(matriz_usuario_filme)
+
+# Recomendando filmes para um cada usuário
+usuarios = at.select("usuarios")
+filmes = at.select("filmes")
+filmes_dict = {filme["id"]: filme["nome"] for filme in filmes}
+for usuario in usuarios:
+    recomendacoes = recomendar_filmes(
+        usuario["id"], matriz_usuario_filme, similaridade_usuarios_df, filmes_dict)
+
+    # substituir id_filme pelo nome do filme
+    print(f"Recomendações para o usuário {usuario['nome']}:")
+    if recomendacoes.empty:
+        continue
+    message = formatar_recomendacoes(recomendacoes, usuario["nome"])
+    send_email(usuario["email"], title=f"Recomendação para {usuario['nome']}",
+               message=message)
